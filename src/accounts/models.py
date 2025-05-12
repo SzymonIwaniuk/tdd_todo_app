19b737943b96f17ee6ca69a13ff33a18573321ff
SzymonIwaniuk <s.f.iwaniuk@gmail.com>
2025-05-12 13:27:14 +0200

    feat: Create a token model to link emails with a unique ID

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,4 +1,6 @@
+import uuid
 from django.db import models
+
 
 class User(models.Model):
     email = models.EmailField(primary_key = True)
@@ -6,3 +8,8 @@ class User(models.Model):
     USERNAME_FIELD = "email"
     is_anonymous = False
     is_authenticated = True
+
+class Token(models.Model):
+    email = models.EmailField()
+    uid = models.CharField(default=uuid.uuid4, max_length=40)
+
--- src/superlists/settings.py
+++ src/superlists/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lists',
-    'accounts'
+    'accounts',
 ]
 
 AUTH_USER_MODEL = "accounts.User"
