5d6ba8ecdf3b96139afc4fc1d13e5b2a8fb19eab
SzymonIwaniuk <s.f.iwaniuk@gmail.com>
2025-05-16 11:55:40 +0200

    feat:🏆 "My lists" working for each unique user - success

--- src/functional_tests/test_my_lists.py
+++ src/functional_tests/test_my_lists.py
@@ -4,6 +4,7 @@ from .container_commands import create_session_on_server
 from .management.commands.create_session import create_pre_authenticated_session
 from selenium.webdriver.common.by import By
 
+
 class MyListsTest(FunctionalTest):
     def create_pre_authenticated_session(self, email: str) -> None:
         if self.test_server:
@@ -22,7 +23,6 @@ class MyListsTest(FunctionalTest):
             )
         )
 
-
     def test_logged_in_users_lists_are_saved_as_my_lists(self) -> None:
         self.create_pre_authenticated_session("user@example.com")
         self.browser.get(self.live_server_url)
@@ -35,10 +35,9 @@ class MyListsTest(FunctionalTest):
         self.wait_for(
             lambda: self.assertIn(
                 "user@example.com",
-                self.browser.find_element(By.CSS_SELECTOR, "h1").text, 
+                self.browser.find_element(By.CSS_SELECTOR, "h2").text,
             )
         )
-
 
         self.wait_for(
             lambda: self.browser.find_element(By.LINK_TEXT, "Reticulate splines")
@@ -53,14 +52,12 @@ class MyListsTest(FunctionalTest):
         self.add_list_item("Click cows")
         second_list_url = self.browser.current_url
 
-
         self.browser.find_element(By.LINK_TEXT, "My lists").click()
         self.wait_for(lambda: self.browser.find_element(By.LINK_TEXT, "Click cows"))
         self.browser.find_element(By.LINK_TEXT, "Click cows").click()
         self.wait_for(
             lambda: self.assertEqual(self.browser.current_url, second_list_url)
         )
-
 
         self.browser.find_element(By.CSS_SELECTOR, "#id_logout").click()
         self.wait_for(
@@ -69,4 +66,3 @@ class MyListsTest(FunctionalTest):
                 [],
             )
         )
-
